a0d76dffe94e6ebe3d0a482c5cedddf7f50a2633
Michael Williamson <mike@zwobble.org>
2013-07-23 15:44:18 +0100

    Fix killing of test server when tests fail

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,8 +3,20 @@
 set -e
 
 THIS_DIR=$(dirname "$0")
+PIDFILE="${THIS_DIR}/_server.pid"
+
+kill_server() {
+    if [ -f $PIDFILE ]; then
+        kill `cat "${PIDFILE}"` || true
+        rm -f "${PIDFILE}"
+    fi
+}
+
+kill_server
 
 node "${THIS_DIR}/server.js" &
 SERVER_PID=$!
+echo ${SERVER_PID} > "${THIS_DIR}/_server.pid"
 "${THIS_DIR}/../node_modules/.bin/grunt" qunit
-kill ${SERVER_PID}
+
+kill_server
