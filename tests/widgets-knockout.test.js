a54ec67f7423befcfe8e0c042692b56ef393f784
Michael Williamson <mike@zwobble.org>
2013-07-23 16:14:07 +0100

    Add test for widget dependencies

--- tests/widgets-knockout.test.js
+++ tests/widgets-knockout.test.js
@@ -17,6 +17,30 @@
         strictEqual(element.textContent, 'Hello Bob');
     });
 
+    test("dependencies of widget are renderable using widget binding", function() {
+        var applicationElement = createEmptyDiv();
+
+        var shoutingWidget = function(options) {
+            options.element.innerHTML = options.name.toUpperCase();
+        };
+
+        var widget = widgetsKnockout.widget({
+            init: function(options) {
+                return {
+                    viewModel: {name: options.name},
+                    template: 'Hello <span data-bind="widget: \'shout\', widgetOptions: {name: name}"></span>'
+                };
+            },
+            dependencies: {
+                shout: shoutingWidget
+            }
+        });
+
+        var element = createEmptyDiv();
+        widget({element: element, name: "Bob"});
+        strictEqual(element.textContent, 'Hello BOB');
+    });
+
 
 
     function createEmptyDiv() {
